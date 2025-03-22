9e181d04a4bfab3dcf8266a8d1cf63536e8db6fd
mouhamed kandji <kandjimouhamed@gmail.com>
2025-03-22 20:58:29 +0000

    add fille toast.php

---
+++ toast.php
@@ -0,0 +1,12 @@
+<div class="toast-container position-fixed top-0 end-0 p-2">
+  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header  bg-secondary text-light">
+      <!-- <img src="..." class="rounded me-2" alt="..."> -->
+      <strong class="me-auto text-light" id="toastMessage"></strong>
+      <!-- <small>11 mins ago</small> -->
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <!-- <div class="toast-body  bg-secondary">
+    </div> -->
+  </div>
+</div>
